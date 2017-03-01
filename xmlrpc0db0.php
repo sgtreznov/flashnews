103d148bacd2d1bd62a26cf183594038c4be5e89
sgtreznov <r_mill33@yahoo.com>
2017-03-01 14:04:20 +0100

    TB

---
+++ xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>http://www.uaeflash247.com/wp</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://www.uaeflash247.com/wp/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://www.uaeflash247.com/wp/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://www.uaeflash247.com/wp/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://www.uaeflash247.com/wp/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://www.uaeflash247.com/wp/?rest_route=/" />
+	    </apis>
+  </service>
+</rsd>
